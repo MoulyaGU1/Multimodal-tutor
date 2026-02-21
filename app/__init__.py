2209e0c4c30b1085b69a6c61c4e7e6f8c7c75a88
MoulyaGU1 <moulyagu@gmail.com>
2026-02-21 11:57:21 +0530

    Removed upload folder creation for Vercel

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,15 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
-import os
-from .config import Config
-
-# Initialize extensions
-db = SQLAlchemy()
-migrate = Migrate()
-
 def create_app():
-    # Load environment variables
     load_dotenv()
 
-    # Create Flask app
     app = Flask(__name__, instance_relative_config=True, template_folder='../templates')
     app.config.from_object(Config)
 
-    # Initialize extensions
-    CORS(app)  # Enable CORS
+    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Ensure upload folder exists
-    upload_folder = app.config.get('UPLOAD_FOLDER')
-    if upload_folder:
-        os.makedirs(upload_folder, exist_ok=True)
-
     with app.app_context():
-        # Import models (important for Flask-Migrate to detect them)
         from . import models
-
-        # Import and register blueprint
         from . import routes
         app.register_blueprint(routes.main)
 
